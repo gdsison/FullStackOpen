79424f1ace3c8af125a5a904882798f78df72382
gdsison <gerard.sison08@gmail.com>
2023-04-29 20:02:22 +0800

    Exercise 3.11

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -7,10 +7,9 @@ const app = express()
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 
 app.use(cors())
-
 app.use(express.json())
-
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.static('build'))
 
 let persons = [
     { 
