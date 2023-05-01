90fdc144be1dea0c1a6743c33ae6a91755b51443
gdsison <gerard.sison08@gmail.com>
2023-05-01 21:42:20 +0800

    Exercise 4.4

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -1,8 +1,26 @@
 const listHelper = require('../utils/list_helper')
+const blogs = require('./blogs')
 
 test('dummy returns one', () => {
   const blogs = []
 
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
+})
+
+describe('total likes', () => {
+  test('of a empty list is zero', () => {
+    const result = listHelper.totalLikes([])
+    expect(result).toBe(0)
+  })
+
+  test.only('when list has only one blog, equals the likes of that', () => {
+    const result = listHelper.totalLikes(Array(blogs[0]))
+    expect(result).toBe(7)
+  })
+
+  test('of a bigger list is calculated right', () => {
+    const result = listHelper.totalLikes(blogs)
+    expect(result).toBe(36)
+  })
 })
