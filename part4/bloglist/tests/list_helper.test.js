79685f00e0f61dfcbc48d117197b901f75e049a6
gdsison <gerard.sison08@gmail.com>
2023-05-01 22:15:36 +0800

    Exercise 4.5

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -14,7 +14,7 @@ describe('total likes', () => {
     expect(result).toBe(0)
   })
 
-  test.only('when list has only one blog, equals the likes of that', () => {
+  test('when list has only one blog, equals the likes of that', () => {
     const result = listHelper.totalLikes(Array(blogs[0]))
     expect(result).toBe(7)
   })
@@ -22,5 +22,32 @@ describe('total likes', () => {
   test('of a bigger list is calculated right', () => {
     const result = listHelper.totalLikes(blogs)
     expect(result).toBe(36)
+  })
+})
+
+describe('favorite blog', () => {
+  test('of a empty list is a empty object', () => {
+    const result = listHelper.favoriteBlog([])
+    expect(result).toEqual({})
+  })
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const favoriteBlog = {
+      title: 'React patterns',
+      author: 'Michael Chan',
+      likes: 7
+    }
+    const result = listHelper.favoriteBlog(Array(blogs[0]))
+    expect(result).toEqual(favoriteBlog)
+  })
+
+  test('of a bigger list is calculated right', () => {
+    const favoriteBlog = {
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      likes: 12
+    }
+    const result = listHelper.favoriteBlog(blogs)
+    expect(result).toEqual(favoriteBlog)
   })
 })
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,7 +6,20 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const returnedBlog = blogs.reduce((max, blog) => max.likes > blog.likes ? max : blog, {})
+
+  const favoriteBlog = {
+    title: returnedBlog.title,
+    author: returnedBlog.author,
+    likes: returnedBlog.likes
+  }
+
+  return favoriteBlog
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
