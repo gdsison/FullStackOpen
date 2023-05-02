350f6c5ccb2135a14e3d00cd89ba12b56fea611b
gdsison <gerard.sison08@gmail.com>
2023-05-02 22:13:29 +0800

    Exercise 4.6

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -28,6 +28,7 @@ describe('total likes', () => {
 describe('favorite blog', () => {
   test('of a empty list is a empty object', () => {
     const result = listHelper.favoriteBlog([])
+    console.log(result)
     expect(result).toEqual({})
   })
 
@@ -49,5 +50,30 @@ describe('favorite blog', () => {
     }
     const result = listHelper.favoriteBlog(blogs)
     expect(result).toEqual(favoriteBlog)
+  })
+})
+
+describe('most blogs', () => {
+  test('of a empty list is a empty object', () => {
+    const result = listHelper.mostBlogs([])
+    expect(result).toEqual({})
+  })
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const mostBlogs = {
+      author: 'Michael Chan',
+      blogs: 1
+    }
+    const result = listHelper.mostBlogs(Array(blogs[0]))
+    expect(result).toEqual(mostBlogs)
+  })
+
+  test('of a bigger list is calculated right', () => {
+    const mostBlogs = {
+      author: 'Robert C. Martin',
+      blogs: 3
+    }
+    const result = listHelper.mostBlogs(blogs)
+    expect(result).toEqual(mostBlogs)
   })
 })
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,4 +1,4 @@
-const dummy = (blogs) => {
+const dummy = () => {
   return 1
 }
 
@@ -9,17 +9,40 @@ const totalLikes = (blogs) => {
 const favoriteBlog = (blogs) => {
   const returnedBlog = blogs.reduce((max, blog) => max.likes > blog.likes ? max : blog, {})
 
-  const favoriteBlog = {
-    title: returnedBlog.title,
-    author: returnedBlog.author,
-    likes: returnedBlog.likes
-  }
+  const favoriteBlog = returnedBlog.title === undefined ? {} :
+    {
+      title: returnedBlog.title,
+      author: returnedBlog.author,
+      likes: returnedBlog.likes
+    }
 
   return favoriteBlog
+}
+
+const mostBlogs = (blogs) => {
+
+  const returnedBlog = blogs.reduce((acc, blogs) => {
+    acc[blogs.author] = (acc[blogs.author] || 0) + 1
+    acc = acc[blogs.author] > acc.maxBlogs
+      ? { maxBlogs : acc[blogs.author], authorMaxBlogs: blogs.author }
+      : acc
+    return acc
+  }, { maxBlogs: 0, authorMaxBlogs: null })
+
+  const mostBlogs = returnedBlog.authorMaxBlogs === null
+    ? {}
+    : {
+      author: returnedBlog.authorMaxBlogs,
+      blogs: returnedBlog.maxBlogs
+    }
+
+  return mostBlogs
+
 }
 
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
