b8ac30d8bc66bda5ea1e8da0bf3cea29cfb8a6ae
gdsison <gerard.sison08@gmail.com>
2023-05-03 18:47:48 +0800

    Exercise 4.8

---
+++ part4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const helper = require('./test_helper')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+beforeAll(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.initialBlogs)
+})
+
+test('blogs are returned as json', async () =>{
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('all blogs are returned', async () => {
+  const blogs = await api.get('/api/blogs')
+
+  expect(blogs.body).toHaveLength(helper.initialBlogs.length)
+})
+
+afterAll(async () => {
+  await mongoose.connection.close()
+})
