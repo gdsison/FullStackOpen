10de37b3c2f671699970a99579092f0f06713d20
gdsison <gerard.sison08@gmail.com>
2023-05-03 20:16:42 +0800

    Exercise 4.10

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -29,6 +29,28 @@ test('blogs unique identifier id is defined', async () => {
   expect(blogs.body[0].id).toBeDefined()
 })
 
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'Facebook',
+    author: 'Mark Zuckerberg',
+    url: 'https://facebook.com',
+    likes: 3
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+  const titles = blogsAtEnd.map(b => b.title)
+  expect(titles).toContain('Facebook')
+
+})
+
 afterAll(async () => {
   await mongoose.connection.close()
 })
