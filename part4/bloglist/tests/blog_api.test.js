da8338337028db07499d6136b791b471a4a243ad
gdsison <gerard.sison08@gmail.com>
2023-05-03 23:13:23 +0800

    Exercise 4.13

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -10,95 +10,123 @@ beforeEach(async () => {
   await Blog.insertMany(helper.initialBlogs)
 })
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+describe('when there is initially some blogs saved', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('all blogs are returned', async () => {
+    const blogs = await api.get('/api/blogs')
+
+    expect(blogs.body).toHaveLength(helper.initialBlogs.length)
+  })
+
+  test('blogs unique identifier (id) is defined', async () => {
+    const blogs = await api.get('/api/blogs')
+
+    expect(blogs.body[0].id).toBeDefined()
+  })
 })
 
-test('all blogs are returned', async () => {
-  const blogs = await api.get('/api/blogs')
+describe('addition of new blog', () => {
+  test('succeeds with valid data', async () => {
+    const newBlog = {
+      title: 'Facebook',
+      author: 'Mark Zuckerberg',
+      url: 'https://facebook.com',
+      likes: 3
+    }
 
-  expect(blogs.body).toHaveLength(helper.initialBlogs.length)
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+    const titles = blogsAtEnd.map(b => b.title)
+    expect(titles).toContain('Facebook')
+  })
+
+  test('succeeds without likes and likes default into 0', async () => {
+    const newBlog = {
+      title: 'Facebook',
+      author: 'Mark Zuckerberg',
+      url: 'https://facebook.com'
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const newBlogAtEnd = await helper.newBlogInDb()
+    expect(newBlogAtEnd.likes).toEqual(0)
+  })
+
+  test('fails with status code 400 if title is missing ', async () => {
+    const newBlog = {
+      author: 'Mark Zuckerberg',
+      url: 'https://facebook.com',
+      likes: 3
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+  })
+
+  test('fails with status code 400 if url is missing', async () => {
+    const newBlog = {
+      title: 'Facebook',
+      author: 'Mark Zuckerberg',
+      likes: 3
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+  })
 })
 
-test('blogs unique identifier id is defined', async () => {
-  const blogs = await api.get('/api/blogs')
+describe('deletion of a blog', () => {
+  test('succeeds with status code 204 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
 
-  expect(blogs.body[0].id).toBeDefined()
-})
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(204)
 
-test('a valid blog can be added', async () => {
-  const newBlog = {
-    title: 'Facebook',
-    author: 'Mark Zuckerberg',
-    url: 'https://facebook.com',
-    likes: 3
-  }
+    const blogsAtEnd = await helper.blogsInDb()
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1)
+  })
 
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+  test('fails with status code 400 if id is invalid', async () => {
+    const randomId = '21332'
+    await api
+      .delete(`/api/blogs/${randomId}`)
+      .expect(400)
 
-  const titles = blogsAtEnd.map(b => b.title)
-  expect(titles).toContain('Facebook')
-})
+    const blogsAtEnd = await helper.blogsInDb()
 
-test('blog without likes is 0', async () => {
-  const newBlog = {
-    title: 'Facebook',
-    author: 'Mark Zuckerberg',
-    url: 'https://facebook.com'
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-  const newBlogAtEnd = await helper.newBlogInDb()
-  console.log(newBlogAtEnd)
-  expect(newBlogAtEnd.likes).toEqual(0)
-})
-
-test('blog without title is not added', async () => {
-  const newBlog = {
-    author: 'Mark Zuckerberg',
-    url: 'https://facebook.com',
-    likes: 3
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
-})
-
-test('blog without url is not added', async () => {
-  const newBlog = {
-    title: 'Facebook',
-    author: 'Mark Zuckerberg',
-    likes: 3
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-
-  const blogsAtEnd = await helper.blogsInDb()
-  console.log(blogsAtEnd)
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+  })
 })
 
 afterAll(async () => {
