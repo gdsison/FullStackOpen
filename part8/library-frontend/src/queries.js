d054430d3524e0503b05d1de70d671d2c43ec1a9
gdsison <gerard.sison08@gmail.com>
2023-05-20 16:30:20 +0800

    Exercise 8.10

--- part8/library-frontend/src/queries.js
+++ part8/library-frontend/src/queries.js
@@ -19,3 +19,14 @@ query AllBooks {
   }
 }
 `
+
+export const CREATE_BOOK = gql`
+mutation Mutation($title: String!, $author: String!, $published: Int!, $genres: [String!]!) {
+  addBook(title: $title, author: $author, published: $published, genres: $genres) {
+    author
+    genres
+    published
+    title
+  }
+}
+`
